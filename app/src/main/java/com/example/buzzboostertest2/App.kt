e3a829a7bddf7281008173d9b3d6d12e5e56507c
Cov Kim <hsco.dev@gmail.com>
2024-01-29 14:33:39 +0900

    오류지점 추가 테스트

--- app/src/main/java/com/example/buzzboostertest2/App.kt
+++ app/src/main/java/com/example/buzzboostertest2/App.kt
@@ -4,8 +4,6 @@ import android.app.Application
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
 
 class App: Application() {
 
@@ -20,12 +18,7 @@ class App: Application() {
 //        BuzzBoosterWrapper.setUser(3216163)
 
         CoroutineScope(Dispatchers.IO).launch {
-            val retrofit = Retrofit.Builder().apply {
-                client(OkHttpClient.Builder().build())
-                baseUrl("0.0.0.0")
-            }.build()
-            val api = retrofit.create(ServiceConfigApi::class.java)
-            api.getConfig()
+            throw Exception()
         }
     }
 }
